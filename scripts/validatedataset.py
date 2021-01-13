6c5285de1faa398ba9afb225188a62b7bc4738f0
Marco Uderzo <47079771+marcouderzo@users.noreply.github.com>
2021-01-13 14:56:47 +0100

    Add files via upload

--- scripts/validatedataset.py
+++ scripts/validatedataset.py
@@ -5,12 +5,31 @@
 
 import os
 
-# Paste your own demo folder
-demospath = "F:/progetto/log/"
+# --------Setup------------
+
+demospath = "F:/progetto/log/"      # Folder where logs are saved
+
+enableLenghtTest = True             # Enable Line Count Test
+linesThreshold = 70000            # Minimum number of lines in log
+
+nPlayers=50                         # number of players you are validating at once
+
+# -------------------------
+
+
 
 isOutputClean=True
-failedLogs=[]
-badLines=[]
+
+zeroedLogs = []
+contaminatedLogs = []
+actionOrEntityMissingLogs = []
+emptyLogs = []
+tooBriefLogs = []
+
+
+print("Do you also want to run Test 3 (Duplicates)? This will take a long time to complete. (y/n)")
+runDupTest = input()
+
 
 print("Running Test 1: Checking for Clean Output...")
 
@@ -22,8 +41,12 @@ for file in os.listdir(demospath):
         file_read = open(demospath+file, 'r')
         lines_read = file_read.readlines() 
         i = 0
+        nullCounter = 0
         for line in lines_read:
             i=i+1
+            if line.find("0.000000 0.000000 0.000000 0.000000")!=-1:
+                nullCounter +=1
+                
             if "Entity" in line:
                 hasEntities = True
                 continue
@@ -33,62 +56,86 @@ for file in os.listdir(demospath):
                 continue
 
             if "Entity" not in line and "Action" not in line:
-                print(file + ": A line is not right!")
-                badLines.append(line)
-                isOutputClean=False
-                failedLogs.append(file)
+                print(file + ": A line does not contain Entity/Action! Line: "+ line)
+                contaminatedLogs.append(file)
                 break
+
+        if(nullCounter > i-(i/3)):
+            print(file + ": Too many null lines (" + str(nullCounter) + "/" + str(i) +" lines)")
+            zeroedLogs.append(file)
 
         if i == 0:
             print(file + ": Empty File!")
-            failedLogs.append(file)
-        elif i < 100000:
+            emptyLogs.append(file)
+        elif i < linesThreshold and enableLenghtTest:
             print(file + ": Log has too few lines.")
-            failedLogs.append(file)
+            tooBriefLogs.append(file)
 
         if not hasEntities or not hasActions and i != 0:
             print(file + ": Missing Actions or Entities!")
-            failedLogs.append(file)
-        
+            actionOrEntityMissingLogs.append(file)
 
 
 print("")
-print("Running Test 2: Checking for Doubled Matches...")
+print("Running Test 2: Checking for Minimum 100 Matches Each Player...")
 
-isDatasetDuplicated = False
-duplicates=[]
+i=0
+enoughMatches = True
+for file in os.listdir(demospath):
+    if "_100" in file:
+        i=i+1
 
-for file1 in os.listdir(demospath):
-    if file1.endswith(".txt"):
-        f1 = open(demospath+file1)
-        for file2 in os.listdir(demospath):
-            if file2.endswith(".txt") and file2 != file1:
-                f2 = open(demospath+file1)
-                if f1.read() == f2.read():
-                    lines_read = f1.readlines()
-                    i = 0
-                    for line in lines_read:
-                        i=i+1
-                    if i == 0:
-                        continue    
-                    duplicates.append(f1)
-                    isDatasetDuplicated=False
-                    print(file1 + " and " + file2 + " : Found a duplicate")
+if i<20:
+    print("Not enough Matches for some players!")        
+    enoughMatches = False
+            
 
+if runDupTest == 'y':
+    print("")
+    print("Running Test 3: Checking for Doubled Matches...")
 
+    duplicates=[]
+
+    for file1 in os.listdir(demospath):
+        if file1.endswith(".txt"):
+            f1 = open(demospath+file1)
+            for file2 in os.listdir(demospath):
+                if file2.endswith(".txt") and file2 != file1:
+                    f2 = open(demospath+file1)
+                    if f1.read() == f2.read():
+                        lines_read = f1.readlines()
+                        i = 0
+                        for line in lines_read:
+                            i=i+1
+                        if i == 0:
+                            continue    
+                        duplicates.append(f1)
+                        print(file1 + " and " + file2 + " : Found a duplicate")
 
             
+
 print("------DATASET VALIDATION REPORT-------")
-if not isOutputClean:
-    print("The following logs are not clean:")
-    print(failedLogs)
-    print("Bad Lines:")
-    print(badLines)
+
+if zeroedLogs:
+    print("Logs With Too Many Zeros: ", zeroedLogs)
+if contaminatedLogs:
+    print("Contaminated Logs: ", contaminatedLogs)
+if actionOrEntityMissingLogs:
+    print("Logs Missing Actions or Entities: ", actionOrEntityMissingLogs)
+if emptyLogs:
+    print("Empty Logs: ", emptyLogs)
+if tooBriefLogs:
+    print("Too Brief Logs", tooBriefLogs)
 else:
     print("All Logs Passed Test 1.")
 
-if isDatasetDuplicated:
-    print("Found a duplicate of the following logs:")
-    print(duplicates)    
+if not enoughMatches:
+    print("Not Enough Matches for some players.")
 else:
-    print("All Logs Passed Test 2.")
+    print("All Players Passed Test 2.")    
+    
+if runDupTest == 'y':
+    if duplicates:
+        print("Duplicates: ", duplicates)
+    else:
+        print("All Logs Passed Test 3.")
